40ef016490711ae7e972b20ab364ed27aaaedc39
ruslan <sqwer1986@gmail.com>
2019-11-19 02:10:17 +0300

    added right_triangle.rb

---
+++ right_triangle.rb
@@ -0,0 +1,21 @@
+sides = []
+
+3.times do |i|
+  puts "Введите #{i + 1}-ю сторону треугольника"
+  sides << side = gets.chomp.to_i
+end
+
+if sides[0] == sides[1] && sides[0] == sides[2]
+  puts 'Треугольник равносторонний'
+elsif sides[0] == sides[1] && sides[0] != sides[2] || sides[2] == sides[1] && sides[2] != sides[0] || sides[0] == sides[2] && sides[0] != sides[1]
+  puts 'Треугольник равнобедренный'
+else
+  max_side = sides.max
+  sides.delete(max_side)
+
+  if max_side**2 == sides[0]**2 + sides[1]**2
+    puts 'Треугольник прямоугольный и разносторонний'
+  else
+    puts 'Треугольник разносторонний'
+  end
+end
