967eaf8a4ba861ebb79c238162e3e3a4b2ad6e94
ruslan <sqwer1986@gmail.com>
2019-11-19 12:06:50 +0300

    change file quadratic_equation.rb

--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -12,11 +12,13 @@ c = gets.chomp.to_f
 
 d = b**2 - 4 * a * c
 
+sq = Math.sqrt(d)
+
 if d < 0
   puts "Дискриминант: #{d} Корней нет"
 elsif d > 0
-  x1 = (-b + Math.sqrt(d)) / (2 * a)
-  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  x1 = (-b + sq) / (2 * a)
+  x2 = (-b - sq) / (2 * a)
   puts "Дискриминант: #{d} Два корня: X1 = #{x1}, X2 = #{x2}"
 else
   x = -b / (2 * a)
